60a140f5cbb7aeba6348b28365a57f8fa71eec5d
sayani2010 <85400495+sayani2010@users.noreply.github.com>
2021-11-09 16:49:18 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -41,3 +41,8 @@ function redirectToRoomName(name)
       localStorage.setItem("room_name " , name);
       window.location ="kwitter_page.html";
 }
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
